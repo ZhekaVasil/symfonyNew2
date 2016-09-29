dbb1fb1abb6b302c67c00b7beb5d956178406ac8
Vista <vistova@gmail.com>
2016-09-29 20:17:17 +0300

    final

--- my_project/src/AppBundle/Controller/InfoController.php
+++ my_project/src/AppBundle/Controller/InfoController.php
@@ -9,13 +9,29 @@ use Symfony\Component\HttpFoundation\Request;
 class InfoController extends Controller
 {
     /**
-     * @Route("/info", name="infopage")
+     * @Route("/create", name="createpage")
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
+       /* // replace this example code with whatever you need
         return $this->render('default/info.html.twig', [
+            
+        ]);*/
 
-        ]);
+
+
+        if ($request->get('submit')) {
+            $name = $request->get('name');
+            $text = $request->get('text');
+            $conn = $this->get('database_connection');
+            $conn->insert('news', array('cat_id' => '1', 'name' => $name, 'text' =>$text ));
+        } else {
+            $name = 'Not submitted yet';
+            $text = 'Not submitted yet';
+        }
+
+        return $this->render('default/info.html.twig', array('name' => $name, 'text' => $text));
+        
     }
+
 }
