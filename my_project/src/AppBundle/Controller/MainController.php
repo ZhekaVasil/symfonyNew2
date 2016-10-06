e171673f588c04476814b9629922877000db03c5
ZhekaVasil <zheka_1408@mail.ru>
2016-10-06 21:31:00 +0300

    final

--- my_project/src/AppBundle/Controller/MainController.php
+++ my_project/src/AppBundle/Controller/MainController.php
@@ -11,13 +11,24 @@ class MainController extends Controller
     /**
      * @Route("/home", name="homepage")
      */
+
     public function indexAction(Request $request)
     {
-        $conn = $this->get('database_connection');
-        $news = $conn->fetchAll('SELECT * FROM news ORDER BY id DESC');
-        // replace this example code with whatever you need
+        if(isset($_POST['login'])){
+            $login = $_POST['login'];
+            $pass = $_POST['password'];
+            $conn = $this->get('database_connection');
+            $news = $conn->fetchAll('SELECT * FROM users WHERE login = ? AND pass = ?', [$login, $pass]);
+            if($news){
+                echo 'OK';
+            } else {
+                echo 'NOT OK';
+            }
+        }
+       /* $conn = $this->get('database_connection');
+        $news = $conn->fetchAll('SELECT * FROM users ORDER BY id DESC');*/
         return $this->render('default/home.html.twig', [
-            'news' => $news,
+           /* 'data' => $news,*/
             'current' => 'main'
         ]);
     }
